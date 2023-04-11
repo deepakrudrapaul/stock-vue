69ab784ef8d6d3508c985af4634a032292611cf6
Deepak Paul <rudra.ad7@gmail.com>
2023-04-12 00:24:19 +0530

    Initial commit

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject } from '@angular/core';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private baseUrl: string = environment.API_ENDPOINT;
+  private apiKey: string = environment.SUPABASE_KEY;
+  private supabase?: SupabaseClient;
+
+
+  constructor() { 
+    this.supabase = new SupabaseClient(this.baseUrl, this.apiKey);
+  }
+
+  getOiData(date: string) { 
+    return this.supabase?.from("bhavcopy").select("*").filter('timestamp', 'eq', date).order('id', {ascending: false}).single()
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_ENDPOINT: "https://cxeowviwukniuzoktejg.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4ZW93dml3dWtuaXV6b2t0ZWpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA5MzA1MDMsImV4cCI6MTk5NjUwNjUwM30.9LFMvjTBtqQAU-73tWEyJA-j-pam-utGsdiXWaL5uy8"
+};
